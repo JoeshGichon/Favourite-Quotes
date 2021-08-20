4b8db0913a0e578c2e0d2e7b3ce41f803b0a1637
Joesh Gichon <joeshgichon56@gmail.com>
2021-08-20 18:05:46 +0300

    Creating an custom model class for quotes that displays the actual quotes as well as quote author when applied by the app.component.ts on app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Favourite-Quotes';
+  quotes:Quote[] = [
+    {quote:"If you're already a front-end developer, well, pretend you're also wearing a pirate hat.",quoteAuthor:" Ethan Marcotte, Responsive Web Design"
+    },
+    {quote:"Let us take you into a deeper experience, make a moment a lasting conveyable memory. Let us help build your tribe.",quoteAuthor:"Deep Immersion"
+    },
+    {quote:"We love what we do and we do what our clients love & work with great clients all over the world to create thoughtful and purposeful websites.",quoteAuthor:"Dr. Christopher Dayagdag"
+    },
+  ]
 }
